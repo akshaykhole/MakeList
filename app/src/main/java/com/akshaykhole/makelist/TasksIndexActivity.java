0e6e32711ffa9daadfef52c1a47ac0eb41b86f3c
Akshay Khole <akshay@reversehack.in>
2016-06-30 14:58:24 -0700

    * SMS receiver with UI updates

--- app/src/main/java/com/akshaykhole/makelist/TasksIndexActivity.java
+++ app/src/main/java/com/akshaykhole/makelist/TasksIndexActivity.java
@@ -1,13 +1,19 @@
 package com.akshaykhole.makelist;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.provider.Telephony;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,7 +23,10 @@ import android.widget.Toast;
 import com.akshaykhole.makelist.adapters.TasksIndexAdapter;
 import com.akshaykhole.makelist.models.Task;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
@@ -33,12 +42,59 @@ public class TasksIndexActivity extends AppCompatActivity implements DialogInter
     private ListView tasksLv;
     private ArrayList<Task> tasksArrayList;
     private RealmResults<Task> tasks;
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
     final private static int REQUEST_CODE_RECEIVE_SMS = 123;
+    final private static String TAG = "**ML**";
 
     @Override
     public void onDismiss(final DialogInterface dialog) {
         populateTasks();
     }
+
+
+    // Define SMS receiver Broadcast receiver
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateTaskList(intent);
+        }
+    };
+
+    private void updateTaskList(Intent intent) {
+        Log.d(TAG, "handling sms");
+        SmsMessage[] smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+        SmsMessage smsMessage = smsMessages[0];
+        String smsFrom = smsMessage.getOriginatingAddress();
+        String smsBody = smsMessage.getMessageBody();
+
+        try {
+            JSONObject mainObject = new JSONObject(smsBody);
+            JSONObject makelistObject = mainObject.getJSONObject("makelist");
+
+            String makelistText = makelistObject.getString("text");
+            String makelistPriority = makelistObject.getString("priority");
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
+            Date tomorrow = calendar.getTime();
+
+            realm = Realm.getDefaultInstance();
+            realm.beginTransaction();
+            Task t = realm.createObject(Task.class);
+            t.setId(UUID.randomUUID().toString());
+            t.setText(makelistText);
+            t.setPriority(makelistPriority);
+            t.setAssignedBy(smsFrom);
+            t.setDueDate(tomorrow);
+            t.setComplete(Boolean.FALSE);
+            realm.commitTransaction();
+        } catch (Exception e){
+            Log.d(TAG + "EXCEPTION->", e.getMessage());
+        }
+
+        populateTasks();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,12 +138,21 @@ public class TasksIndexActivity extends AppCompatActivity implements DialogInter
                 tff.show(fm, "task_form");
             }
         });
+
+        registerReceiver(broadcastReceiver, new IntentFilter(SMS_RECEIVED));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         realm.close();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(broadcastReceiver, new IntentFilter(SMS_RECEIVED));
     }
 
     public void createNewTask(View view) {
