ba955c57879a2236c709b7f3265346261939ba37
Akshay Khole <akshay@reversehack.in>
2016-09-16 02:06:17 -0700

    * Select contact from list of contacts

--- app/src/main/java/com/akshaykhole/makelist/SendTodoActivity.java
+++ app/src/main/java/com/akshaykhole/makelist/SendTodoActivity.java
@@ -1,13 +1,19 @@
 package com.akshaykhole.makelist;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class SendTodoActivity extends AppCompatActivity {
+    static final int REQUEST_SELECT_PHONE_NUMBER = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,4 +33,31 @@ public class SendTodoActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public void selectContact(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_PHONE_NUMBER);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SELECT_PHONE_NUMBER && resultCode == RESULT_OK) {
+            // Get the URI and query the content provider for the phone number
+            Uri contactUri = data.getData();
+            String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
+            Cursor cursor = getContentResolver().query(contactUri, projection,
+                    null, null, null);
+            // If the cursor returned is valid, get the phone number
+            if (cursor != null && cursor.moveToFirst()) {
+                int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(numberIndex);
+                Log.d("MAKELIST-SEND-TODO -> ", number);
+            }
+        }
+    }
+
 }
